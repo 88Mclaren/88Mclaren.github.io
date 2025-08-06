c291f04c2a626911c07f7226a9854c7f09ba4753
88Mclaren <openwheeledracing@gmail.com>
2025-08-05 23:04:46 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+myBtn = document.getElementById('dropDown');
+myNav = document.getElementById('dropDownList');
+
+
+myBtn.onclick = function()
+
+for(var i = true)
+
+if(i == true)
+{
+	myNav.style.display="flex";
+	
+	var i = false
+
+} 
+
+else if (var i = false)
+{
+
+  myNav.style.display ="none"
+
+}
